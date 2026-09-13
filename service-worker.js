dce2f20e262b8078c8454e1ccd4baaec2796ecbf
1492935695-cyber <1492935695@qq.com>
2026-09-14 07:27:51 +0800

    publish service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "luoyi-checkin-v4";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./game.html",
+  "./game.css",
+  "./game.js",
+  "./space-puzzle.html",
+  "./space-puzzle.css",
+  "./space-puzzle.js",
+  "./manifest.webmanifest",
+  "./app-icon.svg",
+  "./robots.txt"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
